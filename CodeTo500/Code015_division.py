a5600a433ae62914d209946897cdbcf82230cd7c
aayush-bhardwaj <aayush.bhj@gmail.com>
2016-12-27 09:32:30 +0530

    Dily analysis management

---
+++ CodeTo500/Code015_division.py
@@ -0,0 +1,21 @@
+#DAY07_24DEC2016_Code015.py
+
+#NOTES
+
+'''
+
+'''
+
+#QUES
+
+'''
+
+'''
+
+#SOLUTION
+
+
+if __name__ == '__main__':
+    s = raw_input()
+    result = swap_case(s)
+    print result
--- DailyAnalysis/ReadMe.md
+++ DailyAnalysis/ReadMe.md
@@ -1,1 +1,3 @@
-ReadMe.md
+This Folder will contain a daily analysis of my sucess stories per day for th enext 50 days . Written everyday @ the EOD .
+
+Cool!
